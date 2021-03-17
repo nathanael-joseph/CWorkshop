74072be3cf0727c1a9cbb79b5b1f9ff75444c03a
Nathanael J Y <nathanaeljy@outlook.com>
2021-03-17 11:57:49 -0400

    started question 2

---
+++ maman11/question2/my_sin.c
@@ -0,0 +1,24 @@
+/*
+-------------------------------------------------------------------------------
+Author: Nathanael J Y
+Last Modified: 17/03/2021
+Written for: The Open University Of Israel
+Course: 20465 - C Programming Workshop
+Assignment: Maman 11 Question 2
+-------------------------------------------------------------------------------
+*/
+
+#include "my_sin.h"
+
+int main() {
+
+	/* print start message */
+
+	/* scan double */
+
+	/* result of my_sin(double d) and sin(double d) */
+
+	printf("the maximum number decimal digits (base-10) that can be represented without change after rounding is \n ");
+	printf("	%d \n", );
+	return 0;
+}
---
+++ maman11/question2/my_sin.h
@@ -0,0 +1,17 @@
+/*
+-------------------------------------------------------------------------------
+Author: Nathanael J Y
+Last Modified: 17/03/2021
+Written for: The Open University Of Israel
+Course: 20465 - C Programming Workshop
+Assignment: Maman 11 Question 2
+-------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+/* ------------------------------ CONSTANTS ------------------------------- */
+#define DBL_ERROR_MARGIN (0.000001)
+#define DBL_PI (3.141592653589793) /* DBL_DIG = 15 */
+
