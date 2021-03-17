fc40276e35c6e01b9171691a7e8ba48166c99e4f
Nathanael J Y <nathanaeljy@outlook.com>
2021-03-17 07:36:56 -0400

    Completed code for Q1

--- maman11/letters.c
+++ maman11/letters.c
@@ -1,46 +1,81 @@
-
+/*
+-------------------------------------------------------------------------------
+Author: Nathanael J Y
+Last Modified: 17/03/2021
+Written for: The Open University Of Israel
+Course: 20465 - C Programming Workshop
+Asiignment: Maman 11 Question 1
+-------------------------------------------------------------------------------
+*/
 
 #include "letters.h"
 
 int main() 
 {
 	/* flags: 1 -> true, 0 -> false */
-	short int isInQuotations = 0;
-	short int isStartOfSentence = 1;
+	int isInQuotations;
+	int isStartOfSentence;
 
 	/* input character */
 	char c;
 
+	/* process the input */
+
+	clearFlag(&isInQuotations);
+	setFlag(&isStartOfSentence);
+
 	while((c = getchar()) != EOF) {
 
+		if (!isWhitespace(c)) {
+			if (c == FULLSTOP) {
+				/* whether or not the full stop is in quotations or not does not change the logic, 
+				   either way, the next letter will be changed to uppercase, and the flag will be cleared */
+				setFlag(&isStartOfSentence);
+			}
+			else {
 
-		if(!isNumber(c)) {
-			
-			if(!isWhitespace(c)) {
-				if (c == FULLSTOP && !isInQuotations) {
-					flipFlag(&isStartOfSentence);
-				} 
-				else if (c == DOUBLEQUOTE) {
+				if (c == DOUBLEQUOTE) {
 					flipFlag(&isInQuotations);
-					isStartOfSentence = 0;
-				} 
+				}
 				else if (isInQuotations || isStartOfSentence ) {
 					c = toUpper(c);
-					isStartOfSentence = 0;
 				}
 				else {
 					c = toLower(c);
 				}
+
+				clearFlag(&isStartOfSentence);
 			}
-			
+		}
+
+		if(!isNumber(c)) {
 			putchar(c);
 		}
-	} 
+
+	} /* ebd while loop */
 
 	return 0;
 }
 
 
+
+/* --- FUNCTION DEFINITIONS -------------------------------- */
+
+
+/* flips an int flag from 1 to 0, or from 0 to 1 */
+void flipFlag(int* flag) {
+	*flag = !*flag;
+}
+
+/* sets an int flag to 1 */
+void setFlag(int* flag) {
+	*flag = 1;
+}
+
+/* sets an int flag to 0 */
+void clearFlag(int* flag) {
+	*flag = 0;
+}
 
 /* returns an Upper Case char of the same letter if applicable, otherwise, returns the argument */
 char toUpper(char c) {
@@ -58,15 +93,8 @@ char toLower(char c) {
 	return c;
 }
 
-
-/* flips a short int flag from 1 to 0, or from 0 to 1 */
-void flipFlag(short int* flag) {
-	static const short int TRUE = 1;
-	*flag = TRUE ^ *flag;
-}
-
-/* returns 1 if c is any of the following {' ', '\t', '\r', '\n', '\v', '\f'} 0 otherwise */
-short int isWhitespace(char c) {
+/* returns 1 if c is any of the following {' ', '\t', '\r', '\n', '\v', '\f'}, 0 otherwise */
+int isWhitespace(char c) {
 	int i;
 	for( i = 0; i < WHITESPACES_ARR_LEN; i++) {
 		if (c == WHITESPACES_ARR[i])
@@ -76,14 +104,14 @@ short int isWhitespace(char c) {
 }
 
 /* returns 1 if c is in the range 'a' -'z', 0 otherwise */
-short int isLowerCase(char c) {
+int isLowerCase(char c) {
 	if(c >= LOWERCASE_START && c <= LOWERCASE_END) 
 		return 1;
 	return 0;
 }
 
 /* returns 1 if c is in the range 'A' -'Z', 0 otherwise */
-short int isUpperCase(char c) 
+int isUpperCase(char c) 
 {
 	if(c >= UPPERCASE_START && c <= UPPERCASE_END) 
 		return 1;
@@ -91,7 +119,7 @@ short int isUpperCase(char c)
 }
 
 /* returns 1 if c is in the range '0' -'9', 0 otherwise */
-short int isNumber(char c) 
+int isNumber(char c) 
 {
 	if(c >= NUMBERS_START && c <= NUMBERS_END)
 		return 1;
--- maman11/letters.h
+++ maman11/letters.h
@@ -1,3 +1,13 @@
+/*
+-------------------------------------------------------------------------------
+Author: Nathanael J Y
+Last Modified: 17/03/2021
+Written for: The Open University Of Israel
+Course: 20465 - C Programming Workshop
+Asiignment: Maman 11 Question 1
+-------------------------------------------------------------------------------
+*/
+
 #include <stdio.h>
 
 /* --- CONSTANTS ------------------------------------------ */
@@ -7,30 +17,34 @@
 #define UPPERCASE_END ('Z')
 #define LOWERCASE_START ('a')
 #define LOWERCASE_END ('z')
-#define WHITESPACES_ARR_LEN (5)
 #define FULLSTOP ('.')
 #define DOUBLEQUOTE ('"')
+#define WHITESPACES_ARR_LEN (5)
 const char WHITESPACES_ARR[] = {' ', '\t', '\r', '\n', '\v'};
 
-typedef enum {false, true} flag;
+
+/* --- FUNCTION DECLARATIONS -------------------------------- */
 
 
-/* --- FUNCTION DEFINITIONS -------------------------------- */
-/* returns 1 if c is any of the following {' ', '\t', '\r', '\n', '\v', '\f'} 0 otherwise */
-short int isWhitespace(char c); 
+/* returns 1 if c is any of the following {' ', '\t', '\r', '\n', '\v', '\f'}, 0 otherwise */
+int isWhitespace(char c); 
+/* returns 1 if c is in the range 'a' -'z', 0 otherwise */
+int isLowerCase(char c);
+/* returns 1 if c is in the range 'A' -'Z', 0 otherwise */
+int isUpperCase(char c);
+/* returns 1 if c is in the range '0' -'9', 0 otherwise */
+int isNumber(char c);
+
 
 /* returns 1 if c is in the range 'a' -'z', 0 otherwise */
-short int isLowerCase(char c);
-
-/* returns 1 if c is in the range 'A' -'Z', 0 otherwise */
-short int isUpperCase(char c);
-
-/* returns 1 if c is in the range '0' -'9', 0 otherwise */
-short int isNumber(char c);
-
-/* flips a short int flag from 1 to 0, or from 0 to 1 */
-void flipFlag(short int* flag);
-
 char toLower(char c);
-
+/* returns 1 if c is in the range 'A' -'Z', 0 otherwise */
 char toUpper(char c);
+
+
+/* flips an int flag from 1 to 0, or from 0 to 1 */
+void flipFlag(int* flag);
+/* sets an int flag to 1 */
+void setFlag(int* flag);
+/* sets an int flag to 0 */
+void clearFlag(int* flag);
