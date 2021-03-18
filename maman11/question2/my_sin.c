5d977d86867db99e05d8dcc3e87c182ce81813c5
Nathanael J Y <nathanaeljy@outlook.com>
2021-03-18 05:51:53 -0400

    finished code for question 2

--- maman11/question2/my_sin.c
+++ maman11/question2/my_sin.c
@@ -1,7 +1,7 @@
 /*
 -------------------------------------------------------------------------------
 Author: Nathanael J Y
-Last Modified: 17/03/2021
+Last Modified: 18/03/2021
 Written for: The Open University Of Israel
 Course: 20465 - C Programming Workshop
 Assignment: Maman 11 Question 2
@@ -12,58 +12,73 @@ Assignment: Maman 11 Question 2
 
 int main() {
 
+	double deg;
+
 	/* print start message */
+	printf("Welcome to my sine calculator. \n");
+	printf("This sine calculator is accurate for all angles in degrees, \n");
+	printf("with and error margin of E-6. \n");
 
 	/* scan double */
+	printf("Enter an angle in degrees: \n >");
+	scanf("%lf", &deg);
 
-	/* result of my_sin(double d) and sin(double d) */
-
-
-	double deg, rads;
-	while(1) {
-		scanf("%lf", &deg);
-		printf("  %.10f degrees is %.10f in radians \n ", deg, toRadians(deg));
-		printf(" sin of  %.10f degrees is %.10f \n", deg, my_sin(toRadians(deg)));
-	}
+	/* Print result of my_sin(double d) and sin(double d) */
+	printf("my_sin of %.6f degrees is %.6f \n", deg, my_sin(toRadians(deg)));
+	printf("math.h sin of %.6f degrees is %.6f \n", deg, sin(toRadians(deg)));
 
 	return 0;
+}
+
+
+/* ------------------------- FUNCTION DEFINITIONS ------------------------ */
+
+/* approximates sin(x) with an error margin less than E-6 */
+double my_sin(double radians) {
+	double result, nextMember, RADIANS_SQRD;
+	int i, denominator;
+
+	/* 
+	to improve runtime and avoid overflow for arguments where |radians| > 2PI,
+	sin(2kPI + x) = sin(x), where |x| < 2PI, k in Z.
+	*/
+	if (absolute(radians) > 2*DBL_PI) {
+		radians = radians - ((int)(radians/(2*DBL_PI))*(2*DBL_PI));
+	}
+
+	/* this value remains constant */
+	RADIANS_SQRD = radians*radians;
+
+	/* the first memeber (i = 0) of the Taylor series is equal to the argument "radians" */
+	result = radians;
+
+	/* calculate the next memeber of the Taylor series */ 
+	i = 1;
+	denominator = (2*i)*((2*i) + 1);	
+	nextMember = (result*RADIANS_SQRD)/denominator;
+
+	/* The absolute value of the member i is always larger than the remainder R_i */
+	while (absolute(nextMember) > DBL_ERROR_MARGIN) {
+		/* if index is odd, subtract next memeber , else add the nest memeber */
+		result += i % 2 == 1 ? -nextMember : nextMember;
+		/* calculate the next memeber of the Taylor series */
+		i++;
+		denominator = (2*i)*((2*i) + 1);
+		nextMember = (nextMember*RADIANS_SQRD)/denominator;
+	}
+
+	return result;
+}
+
+/* returns the absolute value of argument "x" */
+double absolute(double x) {
+	if(x < 0) {
+		return -x;
+	}
+	return x;
 }
 
 /* returns the value of argument "degrees" in radians */
 double toRadians(double degrees) {
 	return degrees*(DBL_PI/180);
-}
-/* approximates sin(x) with an error margin less than E-6 */
-double my_sin(double radians) {
-
-	int i = 1;
-	int denominator = 6;
-
-	/* the first memeber of the summation is radians */
-	double result = radians;
-
-	/* remainder is the absolute value of the next member in the sumation */
-	/* this is always larger than the actual remainder in Legrange form */
-	double remainder = (result*radians*radians)/denominator;
-
-	while (remainder > (double)DBL_ERROR_MARGIN) {
-
-		if (i % 2 == 1) {
-			result -= remainder;
-		} 
-		else {
-			result += remainder;
-		}
-
-		i++;
-		denominator *= (2*i)*((2*i) + 1);
-
-
-		remainder = (result*radians*radians)/denominator;
-		printf(" remainder is %.10f \n", remainder);
-
-	}
-
-
-	return result;
 }
--- maman11/question2/my_sin.h
+++ maman11/question2/my_sin.h
@@ -1,7 +1,7 @@
 /*
 -------------------------------------------------------------------------------
 Author: Nathanael J Y
-Last Modified: 17/03/2021
+Last Modified: 18/03/2021
 Written for: The Open University Of Israel
 Course: 20465 - C Programming Workshop
 Assignment: Maman 11 Question 2
@@ -12,9 +12,12 @@ Assignment: Maman 11 Question 2
 #include <math.h>
 
 /* ------------------------------ CONSTANTS ------------------------------- */
-#define DBL_ERROR_MARGIN (0.000001)
+#define DBL_ERROR_MARGIN (0.000001) /* E-6 */
 #define DBL_PI (3.141592653589793) /* DBL_DIG = 15 */
 
+/* ------------------------- FUNCTION DELCARATIONS ------------------------ */
+/* returns the absolute value of argument "x" */
+double absolute(double x);
 /* returns the value of argument "degrees" in radians */
 double toRadians(double degrees);
 /* approximates sin(x) with an error margin less than E-6 */
