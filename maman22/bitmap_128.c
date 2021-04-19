9cb61149ddb6bca83b0e8c44340af4d47bd57f33
Nathanael J Y <nathanaeljy@outlook.com>
2021-04-19 13:17:19 -0400

    finished bitmap implementation

--- maman22/bitmap_128.c
+++ maman22/bitmap_128.c
@@ -10,24 +10,29 @@ Assignment: Maman 22 Question 1
 
 #include "bitmap_128.h"
 
-/* --- PRIVATE FUNCTION DECLARATIONS -------------------------------- */
-/* --- MAIN --------------------------------------------------------- */
-int main () {
-
-	return 0;
-}
 /* --- FUNCTION DEFINITIONS ----------------------------------------- */
 
+/* returns a pointer to a new insatnce of a bitmap, or null if malloc fails */
+Bitmap_128 *bitmap_128_init() {
+	/* calloc ensures that Words is zero'd out */
+	Bitmap_128 *bm = calloc(sizeof(*bm),1);
+	return bm;
+}
 /* returns the value of a single bit in the bitmap for a given index */
 int getBit(void *bitmap, int index) {
-	/* todo */
-	return 0;
+	Bitmap_128 *bm = bitmap;
+	unsigned int mask = 1 << BIT_OFFSET(index);
+	return ((bm->words[WORD_OFFSET(index)]) & mask) >> BIT_OFFSET(index);
 }
 /* sets a single bit to 1 for a given index in the bitmap */
 void setBit(void *bitmap, int index) {
-
+	Bitmap_128 *bm = bitmap;
+	unsigned int mask = 1 << BIT_OFFSET(index);
+	bm->words[WORD_OFFSET(index)] |= mask;
 }
 /* sets a single bit to 0 for a given index in the bitmap */
-void clearBit(void *bitmap, int index){
-	
+void clearBit(void *bitmap, int index) {
+	Bitmap_128 *bm = bitmap;
+	unsigned int mask = ~(1 << BIT_OFFSET(index));
+	bm->words[WORD_OFFSET(index)] &= mask;
 }
--- maman22/bitmap_128.h
+++ maman22/bitmap_128.h
@@ -1,7 +1,7 @@
 /*
 -------------------------------------------------------------------------------
 Author: Nathanael J Y
-Last Modified: 18/04/2021
+Last Modified: 19/04/2021
 Written for: The Open University Of Israel
 Course: 20465 - C Programming Workshop
 Assignment: Maman 22 Question 1
@@ -13,21 +13,26 @@ Assignment: Maman 22 Question 1
 
 #include <stdlib.h>
 
-/* --- CONSTANTS ------------------------------------------ */
+/* --- CONSTANTS/MACROS ----------------------------------- */
 
 #define WORD_SIZE 	(32)		/* int is 32 bits */
 #define WORD_COUNT	(4)			/* 4*32 = 128 */
+#define WORD_OFFSET(x) (x / WORD_SIZE)
+#define BIT_OFFSET(x) (x % WORD_SIZE) 
 
 /* --- TYPE DEFINITIONS ----------------------------------- */
 
 typedef unsigned int Word;
 
 typedef struct {
-	Word words[WORD_COUNT];
+	Word words[WORD_COUNT]; 
+	/* Word *words; */
 } Bitmap_128;
 
 /* --- FUNCTION DECLARATIONS ------------------------------ */
 
+/* returns a pointer to a new insatnce of a bitmap, or null if malloc fails */
+Bitmap_128 *bitmap_128_init();
 /* returns the value of a single bit in the bitmap for a given index */
 int getBit(void *bitmap, int index);
 /* sets a single bit to 1 for a given index in the bitmap */
