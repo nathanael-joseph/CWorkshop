46ca14d0e3cde8fa7eb6cca1d8d74ed2a20450e5
Nathanael J Y <nathanaeljy@outlook.com>
2021-05-02 00:59:07 -0400

    added useful functions and constants from previous assignment

--- maman22/stringutils.c
+++ maman22/stringutils.c
@@ -11,3 +11,63 @@ Assignment: Maman 22 Question 1
 #include "stringutils.h"
 
 /* --- FUNCTION DEFINITIONS ----------------------------------------- */
+
+/* 
+returns the a new string, equivilant to  the argument string s,
+with all leading and trailing whitespace characters reoved 
+*/
+String string_Trim(const String s) {
+	int len = strlen(s);
+	int left = 0;
+	int right = len - 1;
+
+	String result; 
+
+	while(start < len && isWhitespace(s[left]))
+		left++;
+	while(right > 0  && isWhitespace(s[right]))
+		right--;
+
+	result = calloc(sizeof(*result), right - left + 2);
+
+	/* TODO */
+
+	return result;
+
+
+}
+
+/* TODO - change these function names to reflect that they are for Char only, not strings */
+
+/* returns 1 if c is any of the following {' ', '\t', '\r', '\n', '\v', '\f'}, 0 otherwise */
+Boolean isWhitespace(char c) {
+	int i;
+	for( i = 0; i < WHITESPACES_ARR_LEN; i++) {
+		if (c == WHITESPACES_ARR[i])
+			return true;
+	}
+	return false;
+}
+
+/* returns 1 if c is in the range 'a' -'z', 0 otherwise */
+Boolean isLowerCase(char c) {
+	if(c >= LOWERCASE_START && c <= LOWERCASE_END) 
+		return true;
+	return false;
+}
+
+/* returns 1 if c is in the range 'A' -'Z', 0 otherwise */
+Boolean isUpperCase(char c) 
+{
+	if(c >= UPPERCASE_START && c <= UPPERCASE_END) 
+		return true;
+	return false;
+}
+
+/* returns 1 if c is in the range '0' -'9', 0 otherwise */
+Boolean isNumber(char c) 
+{
+	if(c >= NUMBERS_START && c <= NUMBERS_END)
+		return true;
+	return false;
+}
--- maman22/stringutils.h
+++ maman22/stringutils.h
@@ -14,23 +14,43 @@ Assignment: Maman 22 Question 1
 #include "string.h"
 
 /* --- CONSTANTS ------------------------------------------ */
+
+#define NUMBERS_START ('0')
+#define NUMBERS_END ('9')
+#define UPPERCASE_START ('A')
+#define UPPERCASE_END ('Z')
+#define LOWERCASE_START ('a')
+#define LOWERCASE_END ('z')
+#define WHITESPACES_ARR_LEN (5)
+const char WHITESPACES_ARR[] = {' ', '\t', '\r', '\n', '\v'};
+
 /* --- TYPE DEFINITIONS ----------------------------------- */
 
-typedef *char String;
+typedef char *String;
 
 /* --- FUNCTION DECLARATIONS ------------------------------ */
 
-/* 
-returns the argument string s with all leading and 
-trailing whitespace characters reoved 
-*/
-String string_Trim(String s);
+/* returns true if c is any of the following {' ', '\t', '\r', '\n', '\v', '\f'}, false otherwise */
+Boolean isWhitespace(char c); 
+/* returns 1 if c is in the range 'a' -'z', 0 otherwise */
+Boolean isLowerCase(char c);
+/* returns 1 if c is in the range 'A' -'Z', 0 otherwise */
+Boolean isUpperCase(char c);
+/* returns 1 if c is in the range '0' -'9', 0 otherwise */
+Boolean isNumber(char c);
 
 /* 
-returns an array of strings, where each element is 
-a sdubstring of s, split by the deliminator
+returns the a new string, equivilant to  the argument string s,
+with all leading and trailing whitespace characters reoved 
 */
-String *string_split(String s, char deliminator);
+String string_Trim(const String s);
+
+/* 
+places an array of strings to "out" parameter, where each element is 
+a sdubstring of s, split by the deliminator, 
+and returns the number of elements in the array.
+*/
+int string_split(const String s, char deliminator, String out);
 
 
 #endif
