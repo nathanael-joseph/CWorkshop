26a6b315dd9a37fbf2e66a770e2e4e708937755e
Nathanael J Y <nathanaeljy@outlook.com>
2021-05-22 23:57:26 -0400

    fixed line endings

--- maman23/quine.c
+++ maman23/quine.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char *source = "#include <stdio.h> %c%cint main() { %c%cchar *source = %c%s%c; %c%cprintf(source,10,10,10, 9,34,source,34,10,9,10,9,10);%c%creturn 0; %c}";
-	printf(source,10,10,10, 9,34,source,34,10,9,10,9,10);
+	char *source = "#include <stdio.h> %c%cint main() { %c%cchar *source = %c%s%c; %c%cprintf(source,10,10,10, 9,34,source,34,10,9,10,9,10,10);%c%creturn 0; %c}%c";
+	printf(source,10,10,10, 9,34,source,34,10,9,10,9,10,10);
 	return 0;
 }
